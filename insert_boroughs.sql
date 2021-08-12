8d70ddc0d17b9541d5f8db0cf8560f02ddf74ee4
Alin Stoian [Intern] <Alin.Stoian@endava.com>
2021-08-12 13:05:25 +0300

    Final tuning + Reproiectare + PYTHON

---
+++ insert_boroughs.sql
@@ -0,0 +1,5 @@
+INSERT INTO BOROUGHS (boro_id, name) VALUES (0, 'M');
+INSERT INTO BOROUGHS (boro_id, name) VALUES (1, 'K');
+INSERT INTO BOROUGHS (boro_id, name) VALUES (2, 'B');
+INSERT INTO BOROUGHS (boro_id, name) VALUES (3, 'Q');
+INSERT INTO BOROUGHS (boro_id, name) VALUES (4, 'S');
